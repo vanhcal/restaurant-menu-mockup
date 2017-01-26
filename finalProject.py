39d86f989af61e5d5601fc0d19303d7f10b16248
vanhcal <mellychen87@gmail.com>
2017-01-26 01:40:01 -0800

    Add files via upload

--- finalProject.py
+++ finalProject.py
@@ -1,5 +1,15 @@
-from flask import Flask 
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
